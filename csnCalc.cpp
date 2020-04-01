2e4f398d474550a750bf10dde78ce509ef85a1d2
Carl Henric Prestor <exocorpllc@gmail.com>
2020-04-01 22:02:04 +0200

    Switched lang to English and added menu

--- csnCalc.cpp
+++ csnCalc.cpp
@@ -2,36 +2,71 @@
 
 //Declare func
 int calc(int friBel, int monLef, int income);
+void caseEarnOBel();
 
 int main(){
 
     //Declare variables
-    int userFribel, userMonlef, userIncome, result;
+    int choice;
     
     //Intro
-    std::cout << "Hej och v\x84lkommen till CSN Fribelopps-kalkylator. Ett verktyg som hjälper dig räkna ut hur reducerat dina utbetalningar blir." << std::endl;
+    std::cout << "Hi and welcome to 'CSN-Fribelopp' calculator. A tool designed to help Swedish students." << std::endl;
+    std::cout << "If you're working beside of your studies, this tool will help you to not get into trouble with CSN. " << std::endl;
+
+    //Menu
+    std::cout << "What would you like to calculate?"<< std::endl;
+    std::cout << "1. Earn over 'fribelopp'" << std::endl;
+    std::cout << "2. Reserved" << std::endl;
+    std::cout << "3. Reserved" << std::endl;
+
+    std::cin >> choice;
+    switch(choice)
+    {
+    case 1: caseEarnOBel();
+
+    case 2: 
+
+    case 3: 
+  
+    default: std::cout << "Invalid choice, exiting... " << std::endl;
+        exit(0);   
+   
+    }
+
+}   
+
+void caseEarnOBel(){
+    
+    //Declare variable
+    int userFribel, userMonlef, userIncome, result;
+    
     // ask for input for userFribel
-    std::cout << "Vänligen ange hur mycket fribeloppet är för önskat uträknat kalenderhalvar är." << std::endl;
+    std::cout << "Please enter how much your 'fribelopp' for the term is. " << std::endl;
     std::cin >> userFribel;
     //Ask user for input for userMonlef;
-    std::cout << "Vänligen ange hur många månader av detta kalenderhalvår som kvarstår." << std::endl;
+    std::cout << "Please enter how many payments there is left for the term. " << std::endl;
     std::cin >> userMonlef;
     //Ask user for calculated Income
-    std::cout << "Vänligen ange hur mycket du planerar att tjäna detta kalender halvår." << std::endl;
+    std::cout << "Please enter how much you're planning to earn for the term. " << std::endl;
     std::cin >> userIncome;
     
     //Send info to our calculating function
     result = calc(userFribel, userMonlef, userIncome);
-    std::cout << "CSN kommer dra av " << result << " per resterande utbetalning(ar). ";
-}   
+    std::cout << "CSN will take away: " << result << " from the remaining payment(s)";
+
+    //Stop program
+    exit(0); 
+}
 
 int calc(int friBel, int monLef, int income){
 
     int result;
 
     //Check so that fribel isnt larger than income
-    if(friBel >= income)
+    if(friBel >= income){
         std::cout << "Dina utbetalningar kommer ej bli drabbade";
+        exit(0);
+    }
     else{
         result = income - friBel;
         result = result * 0.61;
